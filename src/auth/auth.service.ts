bdf583f1e47b4ac2f38f8cdd3f64c68ae158bf49
Lasse <l.stausgaard@gmail.com>
2022-04-26 09:38:36 +0200

    Added JWT module, strategy, nest guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,25 +2,29 @@ import {
   ForbiddenException,
   Injectable
 } from '@nestjs/common'
-import { Users, Booking } from '@prisma/client'
+import { User, Booking } from '@prisma/client'
 
 import { PrismaService } from 'src/prisma/prisma.service'
 import { AuthDto } from './dto'
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 import { domainToASCII } from 'url'
+import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class AuthService {
   constructor(
-    private prismaservice: PrismaService
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService
   ) {}
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password)
 
     try {
       const user =
-        await this.prismaservice.users.create({
+        await this.prismaService.user.create({
           data: {
             email: dto.email,
             hash
@@ -47,7 +51,7 @@ export class AuthService {
 
   async signin(dto: AuthDto) {
     const user =
-      await this.prismaservice.users.findUnique({
+      await this.prismaService.user.findUnique({
         where: {
           email: dto.email
         }
@@ -66,8 +70,29 @@ export class AuthService {
       throw new ForbiddenException(
         'credentials incorrect'
       )
-
+    //optional delete user.hash
     delete user.hash
-    return user
+    return this.signToken(user.id, user.email)
+  }
+
+  async signToken(
+    userId: number,
+    email: string
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email: email
+    }
+    const secret =
+      this.configService.get('JWT_SECRET')
+
+    const token = await this.jwtService.signAsync(
+      payload,
+      {
+        expiresIn: '15m',
+        secret: secret
+      }
+    )
+    return { access_token: token }
   }
 }
