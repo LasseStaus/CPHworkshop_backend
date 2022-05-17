fd57279e578a41fb5f602c1c500d8d449fdc4ca1
Johanne Poulsen <johannejpoulsen@gmail.com>
2022-05-17 15:30:16 +0200

    Adds get data for purchases and tickets

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { GetUserId } from 'src/auth/decorator';
 import { TicketDto } from './dto';
 import { TicketService } from './ticket.service';
@@ -8,10 +8,21 @@ export class TicketController {
 
     constructor(private ticketService: TicketService) { }
 
+    @Get('data') // method and path
+    getTickets(@GetUserId() id: string) {
+        // get info about the current user using costume decorator
+        return this.ticketService.getTickets(id)
+    }
+
+    @Get('purchases') // method and path
+    getPurchases(@GetUserId() id: string) {
+        // get info about the current user using costume decorator
+        return this.ticketService.getPurchases(id)
+    }
+
     @Post('purchase')
     @HttpCode(HttpStatus.OK)
     purchaseTicket(@GetUserId() userId: string, @Body() dto: TicketDto) {
-
         return this.ticketService.purchaseTicket(userId, dto)
     }
 
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -8,6 +8,27 @@ export class TicketService {
 
     constructor(private prismaservice: PrismaService) { }
 
+    async getPurchases(userId: string) {
+        const purchases = await this.prismaservice.purchase.findMany({
+            where: {
+                userId: userId
+            }
+        })
+        console.log(purchases);
+
+        return purchases
+    }
+
+
+    async getTickets(userId: string) {
+        const tickets = await this.prismaservice.ticket.findUnique({
+            where: {
+                userId: userId
+            }
+        })
+        return tickets
+    }
+
     async purchaseTicket(userId: string, dto: TicketDto) {
 
         try {
@@ -30,7 +51,9 @@ export class TicketService {
                 }
             })
 
-            await this.prismaservice.$transaction([createTicketPurchase, updateTicket])
+            console.log("vi er her 2");
+
+            return await this.prismaservice.$transaction([createTicketPurchase, updateTicket])
 
         } catch (err) {
             throw new Error('Something went wrong, try agian later')
