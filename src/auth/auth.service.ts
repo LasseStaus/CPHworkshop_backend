5e032e9ef294e12fbe0940fab8e02e78666ef163
Lasse <l.stausgaard@gmail.com>
2022-06-02 19:21:20 +0200

    some small issues

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -156,7 +156,7 @@ export class AuthService {
       })
       return { message: 'BE - RT hash updated successfully' }
     } catch (err) {
-      return { message: 'BE - RT hash update failed' }
+      return { message: 'BE - RT hash update failed', error: err }
     }
   }
 
